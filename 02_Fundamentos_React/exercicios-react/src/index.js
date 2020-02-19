4dca9b715e2ad61eadc7d589776142b8d7b5a992
RafahCSilva <rafahcsilva@gmail.com>
2020-02-18 21:51:01 -0300

    02: O que é JSX?

---
+++ 02_Fundamentos_React/exercicios-react/src/index.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import ReactDom from 'react-dom';
+
+const elemento = document.getElementById('root');
+
+// Passando o texto para ReactDom
+// ReactDom.render('Ola React', elemento);
+
+// Para usar JSX, importe o 'react'
+ReactDom.render(<h1>Ola React</h1>, elemento);
+
+// Estruturas complexas, ao inves de usar JS puro, com appendChild
+// ReactDom.render(<ul>
+//   <li>Pedro</li>
+//   <li>Maria</li>
+//   <li>Ana</li>
+// </ul>, elemento);
+
+// Armazenando numa variavel
+// const jsx = <ul>
+//   <li>Pedro</li>
+//   <li>Maria</li>
+//   <li>Ana</li>
+// </ul>;
+// ReactDom.render(jsx, elemento);
