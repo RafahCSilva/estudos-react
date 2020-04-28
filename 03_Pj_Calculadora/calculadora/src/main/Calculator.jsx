072d8488ba353a0b79203aba292ae6ffb3ed630a
RafahCSilva <rafahcsilva@gmail.com>
2020-04-28 03:05:36 -0300

    03: Implementando a Lógica

--- 03_Pj_Calculadora/calculadora/src/main/Calculator.jsx
+++ 03_Pj_Calculadora/calculadora/src/main/Calculator.jsx
@@ -3,11 +3,21 @@ import './Calculator.css';
 import Button from '../components/Button';
 import Display from '../components/Display';
 
+const initialState = {
+  displayValue: '0',
+  clearDisplay: false,
+  operation: null,
+  values: [0, 0],
+  current: 0,
+};
+
 export default class Calculator extends Component {
+  state = {...initialState};
+
   render() {
     return (
       <div className="calculator">
-        <Display value={100}/>
+        <Display value={this.state.displayValue}/>
 
         <Button label="AC" click={this.clearMemory} triple/>
         <Button label="/" click={this.setOperation} operation/>
@@ -28,7 +38,7 @@ export default class Calculator extends Component {
         <Button label="+" click={this.setOperation} operation/>
 
         <Button label="0" click={this.addDigit} double/>
-        <Button label="," click={this.addDigit}/>
+        <Button label="." click={this.addDigit}/>
         <Button label="=" click={this.setOperation} operation/>
       </div>
     );
@@ -36,14 +46,64 @@ export default class Calculator extends Component {
 
   clearMemory = () => {
     console.log('Limpar');
+    this.setState({...initialState});
   };
 
   setOperation = (operator) => {
     console.log('op:', operator);
+
+    if (this.state.current === 0) {
+      this.setState({
+        operation: operator,
+        current: 1,
+        clearDisplay: true,
+      });
+      return;
+    }
+
+    const equal = operator === '=';
+    const currentOperation = this.state.operation;
+
+    const values = [...this.state.values];
+    values[0] = {
+      '+': (a, b) => a + b,
+      '-': (a, b) => a - b,
+      '*': (a, b) => a * b,
+      '/': (a, b) => a / b,
+    }[currentOperation](values[0], values[1]);
+    values[1] = 0;
+
+    this.setState({
+      displayValue: values[0],
+      operation: equal ? null : operator,
+      current: equal ? 0 : 1,
+      clearDisplay: !equal,
+      values: values,
+    });
   };
 
   addDigit = (digit) => {
     console.log('dig:', digit);
-  };
+    if (digit === '.' && this.state.displayValue.includes('.')) {
+      return;
+    }
 
-}
+    const clearDisplay =
+      this.state.displayValue === '0' ||
+      this.state.clearDisplay;
+    const currentValue = clearDisplay ? '' : this.state.displayValue;
+    const displayValue = currentValue + digit;
+
+    this.setState({displayValue, clearDisplay: false});
+
+    if (digit !== '.') {
+      const i = this.state.current;
+      const newValue = parseFloat(displayValue);
+      const values = [...this.state.values];
+      values[i] = newValue;
+      this.setState({values});
+      console.log(values);
+    }
+
+  };
+};
